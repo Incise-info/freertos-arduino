efa2023d6b21a997778c31c8bd64dc4f68b67136
Shubham Agarwal <Shubham Agarwal>
2017-03-02 13:20:16 +0530

    Tested

---
+++ FreeRtos_Static/FreeRtos_Static/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+croutine.c
+
+event_groups.c
+
+FreeRtos_Static.c
+
+heap_3.c
+
+hooks.c
+
+list.c
+
+port.c
+
+queue.c
+
+tasks.c
+
+timers.c
+
---
+++ FreeRtos_Static/FreeRtos_Static/FreeRtos_Static.c
@@ -0,0 +1,15 @@
+/*
+ * FreeRtos_Static.c
+ *
+ * Created: 02-Mar-17 12:55:29 PM
+ *  Author: Agarwal-pc
+ */ 
+
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}
